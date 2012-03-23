6d1b514de0b289b703e504850798acc59d722798
fab <fdev31@gmail.com>
2012-03-23 19:24:24 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aury",
-    version="0.9",
+    version="1.0",
     author="Fabien Devaux",
     author_email="fdev31@gmail.com",
     license="GPL",
