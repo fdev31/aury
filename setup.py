7454b0fbc5fd4afaa85185947b31053860442a0a
fab <fdev31@gmail.com>
2016-01-30 00:26:19 +0100

    fix tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aury",
-    version="1.0",
+    version="1.1.1",
     author="Fabien Devaux",
     author_email="fdev31@gmail.com",
     license="MIT",
