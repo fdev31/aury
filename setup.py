8cf852d01ead3ad367f9146f1d97409ef637cc4f
fab <fdev31@gmail.com>
2016-01-30 09:21:01 +0100

    Don't need README to install

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
+def getdesc():
+    try:
+        open('README.md').read()
+    except IOError:
+        return 'N/A'
+
 setup(
     name="aury",
-    version="1.1.2",
+    version="1.1.3",
     author="Fabien Devaux",
     author_email="fdev31@gmail.com",
     license="MIT",
     platform="all",
     description="AUR (archlinux) packages maintenance made easy via PyPi lookup",
-    long_description=open('README.md').read(),
+    long_description=getdesc(),
     scripts=['aury'],
     packages=find_packages(),
     url='https://github.com/fdev31/aury',
